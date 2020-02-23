247ea6165c6487290bec4dcbd3a6641030e4fc85
Bobby Pratama <bpratama77@gmail.com>
2020-02-23 21:10:34 +0700

    create form

---
+++ src/components/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="index">
+    <h1>Mini Gallery Kompas</h1>
+    <div>
+      <input type="text" v-model="title" style="margin-bottom:5px" />
+    </div>
+    <div>
+      <input type="text" v-model="description" style="margin-bottom:5px" />
+    </div>
+    <div>
+      <input type="text" v-model="url" style="margin-bottom:5px" />
+    </div>
+
+    <button @click="insertData()">Insert</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data: function() {
+    return {
+      message: "Some Message"
+    };
+  },
+  methods: {}
+};
+</script>
